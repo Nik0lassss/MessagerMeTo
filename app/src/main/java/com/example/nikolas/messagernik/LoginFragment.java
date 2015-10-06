9636681e0d4ac67b59774bf4805630238fab15c3
User <NicholasChirkevich@gmail.com>
2015-10-06 12:23:06 +0300

    add uri to path

--- app/src/main/java/com/example/nikolas/messagernik/LoginFragment.java
+++ app/src/main/java/com/example/nikolas/messagernik/LoginFragment.java
@@ -2,9 +2,12 @@ package com.example.nikolas.messagernik;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -136,11 +139,27 @@ public class LoginFragment extends Fragment implements ServerApi.onUpdateLoginFr
 
         }
     };
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
     View.OnClickListener btnTestLisntenr = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent testIntent = new Intent(getContext(), UploadActivity.class);
-            testIntent.putExtra("filePath", imageUri.getPath());
+            Intent testIntent = new Intent(fragment.getActivity(), UploadActivity.class);
+            String path=getRealPathFromURI(fragment.getActivity(),imageUri);
+            testIntent.putExtra("filePath",path);
+
             testIntent.putExtra("isImage", true);
             startActivity(testIntent);
         }
--- app/src/main/java/com/example/nikolas/messagernik/activity/UploadActivity.java
+++ app/src/main/java/com/example/nikolas/messagernik/activity/UploadActivity.java
@@ -1,11 +1,15 @@
 package com.example.nikolas.messagernik.activity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
